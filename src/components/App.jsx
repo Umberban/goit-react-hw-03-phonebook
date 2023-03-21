1d478e43838aeccbefb035ca427f0910fa742bc1
Umberban <romanovborysv@gmail.com>
2023-03-21 14:36:55 +0200

    finsihed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,20 @@ export class App extends Component{
     filter:'',
   }
 
+  componentDidMount () {
+    const contacts = localStorage.getItem('contacts');
+    const localContacts = JSON.parse(contacts);
+
+    if(localContacts) {
+      this.setState({ contacts: localContacts });
+    };
+  };
+
+  componentDidUpdate (prevProps, prevState) {
+    if(this.state.contacts !== prevState.contacts) {
+     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    };
+  };
 
   addContact = (data)=>{
     const Contact = {
